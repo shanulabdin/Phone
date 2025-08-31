bcffce1dde2ec6be6c5e1120755096019f8293e4
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-08-31 08:16:57 +0500

    Loop with Prompts

--- prompt.js
+++ prompt.js
@@ -1,7 +1,16 @@
+let continueProgram = true;
+
+while (continueProgram){
+  
  const evenOdd = Number(prompt('Write a number:'));
  
  if(evenOdd % 2 === 0){
-   alert(evenOdd + ' is Even')
+   console.log(evenOdd + ' is Even')
  } else if (evenOdd % 2 !== 0){
-   alert(evenOdd + ' is Odd')
+   console.log(evenOdd + ' is Odd')
  }
+ 
+ const userChoice = prompt('do you want to check again? yes/no')
+ 
+ continueProgram = userChoice === 'yes';
+}
