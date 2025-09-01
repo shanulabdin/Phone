958166c5c0d9ef7cf8ba9f55c62cbc77b071c706
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-01 17:20:24 +0500

    update codewars-6.js

--- codewars-6.js
+++ codewars-6.js
@@ -1,19 +1,13 @@
 function digitPowerSum(n) {
-  
-  // your code here
-  let power = 1;
-  let digits = String(n).split('');
-  let raised = 0;
-  
+  let digits = n.toString().split('').map(Number);
+  let sum = 0;
   for(let i = 0; i < digits.length; i++){
-    raised += digits[i] ^ (digits[i].length + 1);
+    sum += digits[i] ** (i + 1);
   }
-  return raised;
+  return sum;
 }
 
 console.log(digitPowerSum(123))  // 1^1 + 2^2 + 3^3 = 32
 console.log(digitPowerSum(456))  // 4^1 + 5^2 + 6^3 = 4 + 25 + 216 = 245
 console.log(digitPowerSum(89))   // 8^1 + 9^2 = 8 + 81 = 89
 
-let num = 3
-console.log(String(num))
