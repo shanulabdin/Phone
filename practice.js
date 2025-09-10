ec4037753442838b305ab3950ad5e5e493f1b5d6
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-09-11 00:33:59 +0500

    update practice.js

--- practice.js
+++ practice.js
@@ -1,30 +1,43 @@
-function findShort(sentence){
-  let words = sentence.split(' ');
-  let short = words[0].length;
+function getCount(sen){
+  let vowels = 'aeiou';
+  let count = 0;
   
-  for(let i = 1; i < words.length; i++){
-    if(words[i].length < short){
-      short = words[i].length;
+  for(let i = 0; i < sen.length; i++){
+    if(vowels.includes(sen[i])){
+      count++;
     }
   }
-  return short;
+  return count;
 }
 
 
-console.log(findShort("bitcoin take over the world maybe who knows perhaps") ) // 3
-console.log(findShort("turns out random test cases are easier than writing out basic ones")) // 3
-console.log(findShort("Let's travel abroad shall we"))  // 2
-
-
-function getSum(a, b){
-  let min = Math.min(a, b);
-  let max = Math.max(a, b);
-  let sum = 0;
+// function findShort(sentence){
+//   let words = sentence.split(' ');
+//   let short = words[0].length;
   
-  for(let i = min; i <= max; i++){
-    sum += i;
-  }
-  return sum;
-}
+//   for(let i = 1; i < words.length; i++){
+//     if(words[i].length < short){
+//       short = words[i].length;
+//     }
+//   }
+//   return short;
+// }
 
-console.log(getSum(2, 8));
+
+// console.log(findShort("bitcoin take over the world maybe who knows perhaps") ) // 3
+// console.log(findShort("turns out random test cases are easier than writing out basic ones")) // 3
+// console.log(findShort("Let's travel abroad shall we"))  // 2
+
+
+// function getSum(a, b){
+//   let min = Math.min(a, b);
+//   let max = Math.max(a, b);
+//   let sum = 0;
+  
+//   for(let i = min; i <= max; i++){
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(getSum(2, 8));
