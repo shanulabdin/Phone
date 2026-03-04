dca228145ed3fe3c4fcc8d6d6c149bfd198e9caa
shanulabdin <209927116+shanulabdin@users.noreply.github.com>
2026-03-04 21:04:21 +0500

    learnes input in uni

---
+++ input.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+# <stdio.h>
+
+int main() {
+    // 	(0°C × 9/5) + 32 = 32°F
+    float c, f;
+    printf("Enter the Celcius Temperature: ")
+    scanf("")
+    printf("Try programiz.pro");
+
+    return 0;
+}
